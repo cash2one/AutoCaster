fbcd2030b6e91a14f396c2acb8af897ddc702642
Abdullah <abdullahm324@gmail.com>
2015-11-12 12:58:20 -0800

    Added killing event / sample game txt

--- Commentator/EventParser.py
+++ Commentator/EventParser.py
@@ -1,12 +1,20 @@
 from threading import Thread;
 import Messages;
 import time;
+import re;
 
 class EventParser(Thread):
-	def __init__(self, eventQueue):
-		Thread.__init__(self);
-		self.eventQueue = eventQueue;
+        def __init__(self, eventQueue):
+                Thread.__init__(self);
+                self.eventQueue = eventQueue;
 
-	def run(self):
-		print "Generating event";
-		self.eventQueue.put(Messages.PropertyChangeMessage("HP", 0, 100));
+        def run(self):
+                print "Generating event";
+                count = 0
+                f = open('game.txt', 'r');
+                for line in f:
+                        if (re.match(".*[^A-z]+Kills_[0-9],[0-9]+", line)):
+                                print line
+                                count += 1
+                print count
+                self.eventQueue.put(Messages.PropertyChangeMessage("HP", 0, 100));
--- TextEngine/script.js
+++ TextEngine/script.js
@@ -2,21 +2,92 @@ var exec = require('child_process').exec;
 var fs = require("fs");
 var Ivona = require("ivona-node");
 
-//exec('calc', function (err, stdout, stderr) {
-//    console.log(stdout);
-//});
-
 var ivona = new Ivona({
 	accessKey: 'GDNAJCIHKDP2YAKG664A',
 	secretKey: '8yaFb9+jGeuI8DDrYKdK+9jUVNAqqYRuxyG254la'
 });
 
-    ivona.createVoice('Kassuhdin got a penta kill.', {
+var lookup = {};
+
+
+function start(message) {
+    
+    if (lookup[message.speech]) {
+        return message.speech
+    }
+
+    var sound = ivona.createVoice(message.speech, {
         body: {
+            parameters: {
+                rate: speech.rate,
+                volume: speech.volume,
+            },
             voice: {
                 name: 'Salli',
                 language: 'en-US',
                 gender: 'Female'
             }
         }
-    }).pipe(fs.createWriteStream('text.mp3'));
+    }).pipe(fs.createWriteStream(message.speech + '.mp3'));
+
+    if (!sound) {
+        //backup MS SAM
+    }
+}
+
+var player = {
+    "name": null, 
+    "champion:": null,
+    "alive": false,
+    "team": 100, // 100 - blue 200 - red
+    "participantID": 0,
+    "health": 0,
+    "mana": 0,
+    "armor": 0,
+    "ad": 0,
+    "ap": 0,
+    "items": [],
+    "spells": [
+        {
+            "id": 0,
+            "cooldown": 0,
+            "level": 0
+        },
+        {
+            "id": 1,
+            "cooldown": 0,
+            "level": 0
+        },
+        {
+            "id": 2,
+            "cooldown": 0,
+            "level": 0
+        },
+        {
+            "id": 3,
+            "cooldown": 0,
+            "level": 0
+        },
+        {
+            "id": 4,
+            "cooldown": 0,
+            "level": 0
+        },
+        {
+            "id": 5,
+            "cooldown": 0,
+            "level": 0
+        }
+    ],
+    "kills": 0,
+    "deaths": 0,
+    "respawn": 0,
+    "assists": 0,
+    "attackspeed": 0,
+    "movement": 0,
+    "mr": 0,
+    "cs": 0,
+    "level": 1,
+    "experience": 0,
+    "gold": 0
+}
