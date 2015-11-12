7b4d1ff1474efb4469142fafc89632c1f2254142
protopizza <aznchipmunk@gmail.com>
2015-11-12 14:16:16 -0800

    audio renderer v2

--- Commentator/AudioPlayer.py
+++ Commentator/AudioPlayer.py
@@ -6,13 +6,19 @@ class AudioPlayer(Thread):
     def __init__(self, rendererQueue):
         Thread.__init__(self)
         self.rendererQueue = rendererQueue
+        self.player = pyglet.media.Player()
 
     def run(self):
         while True:
             event = self.rendererQueue.get(True)
             audio_file = event["audio_file"]
+            volume = event["volume"]
+            message_timestamp = event["timestamp"]
+            now_timestamp = time.time()
 
             # print "Audio player an audio file ({}) to play from source {}.".format(audio_file, event["source"])
 
             media = pyglet.media.load(audio_file)
-            media.play()
+            self.player.volume = volume
+            self.player.queue(media)
+            self.player.play()
--- Commentator/AudioRenderer.py
+++ Commentator/AudioRenderer.py
@@ -5,37 +5,39 @@ import pyvona
 import hashlib
 import os.path
 
+
 AUDIO_DIRECTORY = "./audio/"
 
 class AudioRenderer(Thread):
     def __init__(self, commentatorQueue, rendererQueue):
         Thread.__init__(self)
-
+        self.commentatorQueue = commentatorQueue
+        self.rendererQueue = rendererQueue
         self.v = pyvona.create_voice(
                 "GDNAJCIHKDP2YAKG664A",
                 "8yaFb9+jGeuI8DDrYKdK+9jUVNAqqYRuxyG254la"
             )
         self.v.codec = "ogg"
         self.v.region = "us-west"
-        self.commentatorQueue = commentatorQueue
-        self.rendererQueue = rendererQueue
 
     def run(self):
         while True:
             event = self.commentatorQueue.get(True)
             # print "Audio renderer recieved commentation."
 
+            timestamp = time.time()
             speech = event.text
             print speech
 
             rate = "medium"
-            sha256 = hashlib.sha256(speech + rate)
+            voice_name = "Brian"
+            sha256 = hashlib.sha256(speech + rate + voice_name)
             hash_string = sha256.hexdigest()
 
             output_file = AUDIO_DIRECTORY + hash_string + ".ogg"
             if not os.path.isfile(output_file):
                 self.v.speech_rate = rate
-                self.v.voice_name = "Salli"
+                self.v.voice_name = voice_name
                 self.v.fetch_voice(speech, output_file)
             #else:
                 #print "already exists"
@@ -43,5 +45,7 @@ class AudioRenderer(Thread):
             message = {}
             message["audio_file"] = output_file
             message["source"] = 0
+            message["volume"] = 0.8
+            message["timestamp"] = timestamp
 
             self.rendererQueue.put(message)
