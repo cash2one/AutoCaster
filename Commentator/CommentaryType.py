fa8f3ffbca0c673f5b56a1ddbe3ca37290142c35
protopizza <aznchipmunk@gmail.com>
2015-11-13 08:27:38 -0800

    fixes various

--- Commentator/CommentaryType.py
+++ Commentator/CommentaryType.py
@@ -1,8 +1,9 @@
 
 class CommentaryType:
-	Introduction = 0
-	GameSummary = 1
-	ChampionKill = 2
-	BuildingKill = 3
-	DragonKill = 4
-	BaronKill = 5
+    Introduction = 0
+    GameSummary = 1
+    ChampionKill = 2
+    BuildingKill = 3
+    DragonKill = 4
+    BaronKill = 5
+    LevelUp = 6
